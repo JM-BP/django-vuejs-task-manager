ebdf12f178cc672db909f88e5e30ac3f447058f2
Juan Manuel Balastegui Pérez <jmbp@MacBook-Pro-de-Juanma.local>
2025-03-27 09:55:33 +0100

    Definidos los modelos para tableros, listas y tareas

--- backend/tasks/models.py
+++ backend/tasks/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Board(models.Model):
+    name = models.CharField(max_length=100)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+
+class List(models.Model):
+    name = models.CharField(max_length=100)
+    board = models.ForeignKey(Board, on_delete=models.CASCADE, related_name="lists")
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+    
+class Task(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField(blank=True)
+    completed = models.BooleanField(default=False)
+    list = models.ForeignKey(List, on_delete=models.CASCADE, related_name="tasks")
+    created_at = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
